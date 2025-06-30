8a610c84ad642c7bb4ae2d465308bd6550838534
stssmlr <stanislav.smolyar@gmail.com>
2025-06-30 18:03:49 +0300

    Upgrade frontend, add subscription on fronten

--- react_app/src/pages/ChoosePlanPage.tsx
+++ react_app/src/pages/ChoosePlanPage.tsx
@@ -27,7 +27,7 @@ const ChoosePlanPage = () => {
 
   return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-bold mb-4">Оберіть свій план</h2>
+      <h2 className="text-3xl font-bold mb-4">Оберіть план</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl mb-6">
         {plans.map((plan) => (
           <div
