a5b18db107507129992daaeb6f3b56f2aec2954f
Ruzhevych Veniamin <ben.ruzhevych@icloud.com>
2025-06-30 13:02:10 +0300

    Add validators and JWT

---
+++ Diplomna_Netflix/Core/Services/JwtService.cs
@@ -0,0 +1,48 @@
+using Core.Options;
+using Data.Entities.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Services
+{
+    public class JwtService
+    {
+        private readonly JwtOptions _options;
+
+        public JwtService(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public string GenerateToken(UserEntity user, IList<string> roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName ?? ""),
+                new Claim(ClaimTypes.Email, user.Email ?? "")
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_options.ExpirationHours),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- Diplomna_Netflix/Diplomna_Netflix/Controllers/Users/UsersController.cs
+++ Diplomna_Netflix/Diplomna_Netflix/Controllers/Users/UsersController.cs
@@ -1,9 +1,11 @@
 using Core.DTOs.UsersDTOs;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Diplomna_Netflix.Controllers.Users
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
