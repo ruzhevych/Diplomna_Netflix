a449383e471ab3d1a9e3c2e4b315dd892d249bf9
stssmlr <stanislav.smolyar@gmail.com>
2025-09-24 23:21:45 +0300

    Movies, Series Page

--- react_app/tailwind.config.js
+++ react_app/tailwind.config.js
@@ -8,28 +8,16 @@ module.exports = {
     extend: {
       animation: {
         fadeIn: 'fadeIn 1s ease-in-out',
-        fontFamily: {
-
-        unbounded: ["Unbounded", "sans-serif"], 
-        
       },
-      maxWidth: {
-        '8xl': '1440px',
-        '9xl': '1600px',
-      }
-
-          sans: ["Instrument Sans", "sans-serif"], // тепер font-sans = Instrument Sans
-        },
-
+      fontFamily: {
+        unbounded: ["Unbounded", "sans-serif"], 
+        sans: ["Instrument Sans", "sans-serif"], 
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0, transform: 'translateY(20px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
         },
-      },
-      fontFamily: {
-        sans: ["Instrument Sans", "sans-serif"], // <-- правильно тут
       },
     },
   },
